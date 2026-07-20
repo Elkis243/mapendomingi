dc925089a72eb181d026520f394eacb512850731
Elkis243 <elielubama@gmail.com>
2026-07-20 14:25:31 +0100

    Initialize Django project structure with base settings, URL configuration, and management script. Add ASGI and WSGI configurations for deployment. Include local and production settings files.

---
+++ congif/settings/local.py
@@ -0,0 +1,7 @@
+"""
+Django local development settings.
+"""
+
+from .base import *  # noqa: F401, F403
+
+DEBUG = True
---
+++ congif/settings/production.py
@@ -0,0 +1,9 @@
+"""
+Django production settings.
+"""
+
+from .base import *  # noqa: F401, F403
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
